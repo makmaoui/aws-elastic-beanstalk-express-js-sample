35e8c127f2689c188341eaa18203f75eff8fe282
Rachid Hammaoui <rachid.hammaoui@accenture.com>
2021-08-13 22:37:40 +0200

    Edit message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Rachid!'));
+app.get('/', (req, res) => res.send('Hello Rachid Pipeline terminé!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
